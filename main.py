a2c062e70c5b1a92a6b9eb4499ecc2220d4ef2d6
iltison <ilyushka123456@gmail.com>
2021-05-14 12:39:38 +0500

    application with documentation

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from PyQt5 import QtWidgets, QtCore,QtGui
+from PyQt5.QtWidgets import QApplication, QTableView, QWidget, QGridLayout, QPushButton,QItemDelegate
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
 import numpy as np
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QPushButton, QLineEdit, QVBoxLayout, QMessageBox
-class Delegate(QtWidgets.QItemDelegate):
-    def __init__(self, owner, choices):
+import pyqtgraph as pg
+import h5py
+
+class Delegate(QItemDelegate):
+    def __init__(self, owner):
         super().__init__(owner)
-        self.items = choices
+        self.items = ['1', '2', '3','4','5']
 
     def createEditor(self, parent, option, index):
         self.editor = QtWidgets.QComboBox(parent)
         self.editor.addItems(self.items)
         return self.editor
+
+    def setEditorData(self, editor, index):
+        index.model().table[index.row()][index.column()] = editor.currentText()
 
     def setModelData(self, editor, model, index):
         value = editor.currentText()
@@ -20,6 +28,8 @@ class Model(QtCore.QAbstractTableModel):
     def __init__(self, table):
         super().__init__()
         self.table = table
+        self.selected_column_arg = 1
+        self.selected_column_sum = 0
 
     def rowCount(self, parent):
         return len(self.table)
@@ -32,7 +42,18 @@ class Model(QtCore.QAbstractTableModel):
 
     def data(self, index, role):
         if role == QtCore.Qt.DisplayRole:
-            return str(self.table[index.row()][index.column()])
+            column = index.column()
+            row = index.row()
+            if column == 5:
+                self.table[row][column] = self.table[row,self.selected_column_arg]
+                return str(self.table[row][column])
+
+            if column == 6:
+                self.table[row][column] = self.table[:,self.selected_column_sum].sum()
+                return str(self.table[row][column].round(4))
+
+            else:
+                return str(self.table[row][column])
 
     def setData(self, index, value, role):
         def is_digit(s):
@@ -48,55 +69,59 @@ class Model(QtCore.QAbstractTableModel):
         if not index.isValid():
             return False
 
-        #if role != QtCore.Qt.EditRole:
-            #return False
-
-        self.table[index.row()][index.column()] = value
-        self.table[:,5] = self.table[:,1]                           
-        self.table[:,6] = self.table[:,0].sum()   
-
+        self.table[index.row()][index.column()] = value 
         self.dataChanged.emit(index, index)
         return True
 
-class Main(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self,parent)
-        # set combo box choices:
-        choices = ['1', '2', '3','4','5']
-        # create table data:
-        data1 = np.random.random(size=(5,5)).round(4)
-        data2 = np.zeros((5,2))
-        table = np.concatenate((data1, data2),axis = 1)
-        table[:,1] = np.ones(5)
-        # create table view:
-        
+class Table(QWidget):
+    def __init__(self,parent=None):
+        super().__init__()
+        self.setFixedSize(740, 700)
+        table = self.load_file(reading_from_file=False)
         self.model = Model(table)
-
-        self.tableView = QtWidgets.QTableView()
-        self.tableView.setModel(self.model)
-        self.tableView.setItemDelegateForColumn(1, Delegate(self,choices))
-        self.tableView.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContentsOnFirstShow)
-        self.tableView.horizontalHeader().setStretchLastSection(True )
-        
-        # make combo boxes editable with a single-click:
+        self.table = QTableView()
+        self.table.setModel(self.model)
+        self.table.setItemDelegateForColumn(1, Delegate(self))
         for row in range(len(table)):
-            self.tableView.openPersistentEditor(self.model.index(row, 1))
-            #self.model.setData(self.model.index(row, 1), choices[0], QtCore.QVariant(self.model.index(row, 1)))
-        # initialize
+            self.table.openPersistentEditor(self.model.index(row, 1))
 
-        self.button = QPushButton("Push for Window",self)
-        self.button.resize(100,32)
-        self.button.move(50, 180) 
+        self.btnLoad = QPushButton("save")
+        self.btnLoad.clicked.connect(self.save_file)
 
-        self.setCentralWidget(self.tableView)
-        #self.setCentralWidget(self.button)
+        self.grid = QGridLayout(self)
+        self.grid.addWidget(self.table,0,0,5,5)
+        self.grid.addWidget(self.btnLoad,5,0,1,1)
         
+        self.graphWidget = pg.PlotWidget()
+        self.grid.addWidget(self.graphWidget,2,0,3,5)
+        
+        self.btn_graph= QPushButton("Graph")
+        self.btn_graph.clicked.connect(self.draw_graph)
+        self.grid.addWidget(self.btn_graph,5,1,1,1)
 
-    def button_clicked():
-        print("Button 1 clicked")
-if __name__ == '__main__':
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    main = Main()
-    main.show()
+    def load_file(self, reading_from_file = True):
+        if reading_from_file:
+            with h5py.File('file.hdf5', 'r') as f:
+                dataset = f['table_array'][:]
+                return dataset
+        else:
+            dataset = np.random.random(size=(5,7)).round(4)
+            return dataset
+
+    def draw_graph(self):
+        self.graphWidget.clear()
+        rows = [index.column() for index in self.table.selectedIndexes()]
+        rows = list(dict.fromkeys(rows))
+        x = self.model.table[:,rows[-1]]
+        y = self.model.table[:,rows[-2]]
+        self.graphWidget.plot(x, y)
+    
+    def save_file(self):
+        with h5py.File('file.hdf5', 'w') as hf:
+            hf.create_dataset("table_array",  data=self.model.table)
+        
+if __name__=="__main__":
+    app = QApplication([])
+    w = Table()
+    w.show()
     app.exec_()
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 from PyQt5.QtWidgets import QApplication, QTableView, QWidget, QGridLayout, QPushButton,QItemDelegate
-from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 import numpy as np
@@ -7,16 +6,45 @@ import pyqtgraph as pg
 import h5py
 
 class Delegate(QItemDelegate):
-    def __init__(self, owner, choices):
+    def __init__(self, owner):
         super().__init__(owner)
-        self.items = choices
+        self.items = ['1', '2', '3','4','5']
 
     def createEditor(self, parent, option, index):
+        """
+        Создает поле со списком внутри родительского элемента
+
+        args:
+            parent : контейнер с выпадающем списком
+        not used args:
+            option : параметры, использованные для рисования элемента в виде виджета
+            index : индекс, в котором поле со списком будет добавлено
+        return:
+            self.editor : выпадающий список 
+        """
         self.editor = QtWidgets.QComboBox(parent)
         self.editor.addItems(self.items)
         return self.editor
 
+    def setEditorData(self, editor, index):
+        """
+        Изменяет при создание значения таблицы на значения выпадающего списка 
+
+        args:
+            editor : поле со списком
+            index : индекс, в котором находится поле со списком
+        """
+        index.model().table[index.row()][index.column()] = editor.currentText()
+
     def setModelData(self, editor, model, index):
+        """
+        Изменяет значения таблицы на значения полученные из выпадающего меню
+
+        args:
+            editor : поле со списком
+            model : таблица
+            index : индекс, в котором находится поле со списком
+        """
         value = editor.currentText()
         model.setData(index, value, QtCore.QVariant(value))
     
@@ -25,21 +53,72 @@ class Model(QtCore.QAbstractTableModel):
     def __init__(self, table):
         super().__init__()
         self.table = table
+        self.selected_column_arg = 1
+        self.selected_column_sum = 0
+
+        self.arg_column = 5
+        self.sum_column = 6
 
     def rowCount(self, parent):
+        """
+        Получение количества строк таблицы
+        """
         return len(self.table)
 
     def columnCount(self, parent):
+        """
+        Получение количества столбцов таблицы
+        """
         return len(self.table[0])
 
     def flags(self, index):
-        return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+        """
+        Доступные флаги для ячеек
+
+        args:
+            index : индекс, в котором находится поле
+        return:
+            возвращает флаги
+        """
+        column = index.column()
+        if column in (self.arg_column,self.sum_column):
+            return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable 
+        else:
+            return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable 
 
     def data(self, index, role):
-        if role == QtCore.Qt.DisplayRole:
-            return str(self.table[index.row()][index.column()])
+        """
+        Получение значения массива соответствующее индексу поля таблицы
+
+        args:
+            index : индекс, в котором находится поле
+            role : роль с указанием типа данных
+        return:
+            значение массивы соответствующее индексу поля
+        """
+        if role == QtCore.Qt.DisplayRole or role == QtCore.Qt.EditRole:
+            column = index.column()
+            row = index.row()
+            if column == self.arg_column:
+                self.table[row][column] = self.table[row,self.selected_column_arg]
+                return str(self.table[row][column])
+
+            if column == self.sum_column:
+                self.table[row][column] = self.table[:,self.selected_column_sum].sum()
+                return str(self.table[row][column].round(4))
+
+            else:
+                return str(self.table[row][column])
 
     def setData(self, index, value, role):
+        """
+        Отображение значений в таблице
+
+        args:
+            index : индекс, в котором находится поле
+            value : значение, которое установится в поле
+            role : роль с указанием типа данных
+        """
         def is_digit(s):
             try:
                 float(s)
@@ -53,61 +132,103 @@ class Model(QtCore.QAbstractTableModel):
         if not index.isValid():
             return False
 
-        #if role != QtCore.Qt.EditRole:
-            #return False
-
-        self.table[index.row()][index.column()] = value
-        self.table[:,5] = self.table[:,1]                           
-        self.table[:,6] = self.table[:,0].sum()   
-
+        self.table[index.row()][index.column()] = value 
         self.dataChanged.emit(index, index)
         return True
 
-class Table(QWidget):
-    def __init__(self,parent=None):
-        super().__init__()
+class Ui_MainWindow(object):
+    """
+    Класс для создания объектов интерфейса 
+    """
+    def setupUi(self, MainWindow):
+        
         self.setFixedSize(740, 700)
-        choices = ['1', '2', '3','4','5']
-        data1 = np.random.random(size=(5,5)).round(4)
-        data2 = np.zeros((5,2))
-        table = np.concatenate((data1, data2),axis = 1)
-        table[:,1] = np.ones(5)
-
-        self.model = Model(table)
-        self.table = QTableView()
-        self.table.setModel(self.model)
-        self.table.setItemDelegateForColumn(1, Delegate(self,choices))
-        for row in range(len(table)):
-            self.table.openPersistentEditor(self.model.index(row, 1))
-
-        self.btnLoad = QPushButton("load")
-        self.btnLoad.clicked.connect(self.load_data)
 
         self.grid = QGridLayout(self)
-        self.grid.addWidget(self.table,0,0,5,5)
-        self.grid.addWidget(self.btnLoad,5,0,1,1)
-        
-        self.graphWidget = pg.PlotWidget()
-        self.grid.addWidget(self.graphWidget,2,0,3,5)
-        
+        self.table = QTableView()
+        self.btn_load = QPushButton("Save")
         self.btn_graph= QPushButton("Graph")
-        self.btn_graph.clicked.connect(self.draw_graph)
+        self.graphWidget = pg.PlotWidget()
+
+        self.grid.addWidget(self.table,0,0,5,5)
+        self.grid.addWidget(self.graphWidget,2,0,3,5)
+        self.grid.addWidget(self.btn_load,5,0,1,1)
         self.grid.addWidget(self.btn_graph,5,1,1,1)
+
+class main_window(Ui_MainWindow, QWidget):
+    """
+    Класс главного окна 
+    """
+
+    def __init__(self, parent=None):
+        """Инициализация GUI
+
+        args:
+        self : Виджеты
+        """
+        QWidget.__init__(self, parent)
+        self.setupUi(self)
         
+        self.reading_file=False
+
+        table = self.load_data(self.reading_file)
+        self.model = Model(table)
+        self.table.setModel(self.model)
+        self.table.setItemDelegateForColumn(1, Delegate(self))
+        for row in range(len(table)):
+            self.table.openPersistentEditor(self.model.index(row, 1))
+        self.table.setSelectionBehavior(QTableView.SelectColumns)
+        self.table.setSelectionMode(QTableView.ContiguousSelection)
+        self.btn_load.clicked.connect(self.save_file)
+        self.btn_graph.clicked.connect(self.draw_graph)
+
+    def load_data(self, reading_from_file = True):
+        """
+        Функция загрузки датасета из файла или созданиние нового с случайными значениями 
+
+        args:
+            self : main_window object
+            reading_from_file : True - чтение из файла
+                                False - создание нового массива
+        """
+        if reading_from_file:
+            with h5py.File('file.hdf5', 'r') as f:
+                dataset = f['table_array'][:]
+                return dataset
+        else:
+            dataset = np.random.random(size=(5,7)).round(4)
+            return dataset
+
     def draw_graph(self):
-        self.graphWidget.clear()
-        rows = [index.column() for index in self.table.selectedIndexes()]
-        rows = list(dict.fromkeys(rows))
-        x = self.model.table[:,rows[-1]]
-        y = self.model.table[:,rows[-2]]
-        self.graphWidget.plot(x, y)
+        """
+        Функция создания графика
+
+        args:
+            self : main_window object
+        """
+        try:
+            self.graphWidget.clear()
+            columns = [index.column() for index in self.table.selectedIndexes()]
+            columns = list(dict.fromkeys(columns))
+            x = self.model.table[:,columns[-1]]
+            y = self.model.table[:,columns[-2]]
+            self.graphWidget.plot(x, y)
+
+        except:
+            raise Exception('select 2 columns') 
     
     def save_file(self):
-        with h5py.File('file.h5', 'w') as hf:
+        """
+        Функция сохранения массива
+
+        args:
+            self : main_window object
+        """
+        with h5py.File('file.hdf5', 'w') as hf:
             hf.create_dataset("table_array",  data=self.model.table)
         
 if __name__=="__main__":
     app = QApplication([])
-    w = Table()
+    w = main_window()
     w.show()
     app.exec_()
