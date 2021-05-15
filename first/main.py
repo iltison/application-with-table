33cb43dc3df123dba438261922d668272d1358ba
iltison <ilyushka123456@gmail.com>
2021-05-15 13:34:35 +0500

    application full

--- first/main.py
+++ first/main.py
@@ -1,10 +1,12 @@
-from PyQt5.QtWidgets import QApplication, QTableView, QWidget, QGridLayout, QPushButton,QItemDelegate
+from PyQt5.QtWidgets import QApplication, QTableView, QWidget, QGridLayout, QPushButton,QItemDelegate,QFileDialog
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5 import QtGui 
 import numpy as np
 import pyqtgraph as pg
 import h5py
+
+from setting import size_array, preload_read_file, preload_filename
 
 class Delegate(QItemDelegate):
     def __init__(self, owner):
@@ -35,7 +37,10 @@ class Delegate(QItemDelegate):
             editor : поле со списком
             index : индекс, в котором находится поле со списком
         """
-        index.model().table[index.row()][index.column()] = editor.currentText()
+        
+        value = editor.currentText()
+        model = index.model()
+        model.table[index.row()][index.column()] = value
 
     def setModelData(self, editor, model, index):
         """
@@ -46,8 +51,11 @@ class Delegate(QItemDelegate):
             model : таблица
             index : индекс, в котором находится поле со списком
         """
+
+        index_arg = model.index(index.row(), model.arg_column)
         value = editor.currentText()
         model.setData(index, value, QtCore.QVariant(value))
+        model.setData(index_arg, value, QtCore.QVariant(value))
     
     
 class Model(QtCore.QAbstractTableModel):
@@ -76,7 +84,15 @@ class Model(QtCore.QAbstractTableModel):
         return cnt_columns
 
     def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
+        """
+        Получение названия заголовка
 
+        args:
+            section : секция
+            orientation : ориентация
+        return:
+            возвращает название заголовка
+        """
         if role == QtCore.Qt.DisplayRole and orientation == QtCore.Qt.Horizontal:
             if section == self.sum_column:
                 return 'Сумма 1-ого столбца'
@@ -99,7 +115,7 @@ class Model(QtCore.QAbstractTableModel):
         else:
             return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable 
 
-    def data(self, index, role):
+    def data(self, index, role=QtCore.Qt.DisplayRole):
         """
         Получение значения массива соответствующее индексу поля таблицы
 
@@ -166,13 +182,15 @@ class Ui_MainWindow(object):
 
         self.grid = QGridLayout(self)
         self.table = QTableView()
-        self.btn_load = QPushButton("Save")
+        self.btn_save = QPushButton("Save")
+        self.btn_load= QPushButton("Load")
         self.btn_graph= QPushButton("Graph")
         self.graphWidget = pg.PlotWidget()
 
         self.grid.addWidget(self.table,0,0,4,4)
         self.grid.addWidget(self.graphWidget,4,0,2,4)
-        self.grid.addWidget(self.btn_load,6,0,1,2)
+        self.grid.addWidget(self.btn_save,6,0,1,2)
+        self.grid.addWidget(self.btn_load,7,0,1,4)
         self.grid.addWidget(self.btn_graph,6,2,1,2)
 
 class main_window(Ui_MainWindow, QWidget):
@@ -188,63 +206,106 @@ class main_window(Ui_MainWindow, QWidget):
         """
         QWidget.__init__(self, parent)
         self.setupUi(self)
-        
-        self.reading_file=False
-        self.filename = 'first/file.hdf5'
+        load_table_data = self.preload_data()
+        self.create_table(load_table_data)
 
-        table = self.load_data(self.reading_file)
-        self.model = Model(table)
+        self.btn_save.clicked.connect(self.save_button_handler)
+        self.btn_graph.clicked.connect(self.graph_button_handler)
+        self.btn_load.clicked.connect(self.load_button_handler)
+
+    def create_table(self, table_data):
+        """
+        Создание таблицы 
+
+        args:
+        table_data : данные для таблицы
+        """
+        self.model = Model(table_data)
         self.table.setModel(self.model)
         self.table.setItemDelegateForColumn(1, Delegate(self))
-        for row in range(len(table)):
+        for row in range(len(table_data)):
             self.table.openPersistentEditor(self.model.index(row, 1))
         self.table.setSelectionBehavior(QTableView.SelectColumns)
-        #self.table.setSelectionMode(QTableView.ContiguousSelection)
-
         self.table.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
         self.table.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
-
-        self.btn_load.clicked.connect(self.save_file)
-        self.btn_graph.clicked.connect(self.draw_graph)
-
         header = self.table.horizontalHeader()
-        header.setSectionResizeMode(self.model.arg_column, QtWidgets.QHeaderView.ResizeToContents) 
-        header.setSectionResizeMode(self.model.sum_column, QtWidgets.QHeaderView.ResizeToContents)        
-        
+        for column in range(table_data.shape[1]):
+            header.setSectionResizeMode(column, QtWidgets.QHeaderView.ResizeToContents)
+        #header.setSectionResizeMode(self.model.arg_column, QtWidgets.QHeaderView.ResizeToContents) 
+        #header.setSectionResizeMode(self.model.sum_column, QtWidgets.QHeaderView.ResizeToContents)  
 
-    def load_data(self, reading_from_file = True):
+    def preload_data(self):
         """
-        Функция загрузки датасета из файла или созданиние нового с случайными значениями 
+        Загрузка начального датасета
+        
+        preload_read_file : True - чтение из файла
+                            False - создание нового массива
+        preload_filename : Расположение файла с датасетом 
+        настройки расположены в файле setting
+            
+        """
+        if preload_read_file:
+            dataset = self.read_dataset_file(preload_filename)
+        else:
+            dataset = self.create_random_array()
+        return dataset
+
+    def read_dataset_file(self, filename):
+        """
+        Чтение файла
 
         args:
-            self : main_window object
-            reading_from_file : True - чтение из файла
-                                False - создание нового массива
+            filename : расположение файла 
         """
-        if reading_from_file:
-            with h5py.File(self.filename, 'r') as f:
-                dataset = f['table_array'][:]
-                return dataset
-        else:
-            dataset = np.random.randint(low=-10, high=10,size=(8,4))
+        with h5py.File(filename, 'r') as f:
+            dataset = f['table_array'][:]
             return dataset
 
-    def save_file(self):
+    def save_dataset_file(self, filename):
         """
-        Функция сохранения массива
+        Сохранение файла
 
         args:
-            self : main_window object
+            filename : расположение файла 
         """
-        with h5py.File(self.filename, 'w') as hf:
+        with h5py.File(filename, 'w') as hf:
             hf.create_dataset("table_array",  data=self.model.table)
 
-    def draw_graph(self):
+    def create_random_array(self):
         """
-        Функция создания графика
+        Создание рандомного массива
 
-        args:
-            self : main_window object
+        size_array: размер массива
+        настройка расположена в файле setting
+
+        return:
+            dataset : массив со случайными значениями
+        """
+        dataset = np.random.randint(low=-1000, high=1000, size=size_array)
+        return dataset
+
+    def load_button_handler(self):
+        """
+        Обработчик кнопки загрузки
+        """
+        try:
+            filename = self.openFileNameDialog()
+            load_table_data = self.read_dataset_file(filename=filename)
+            self.create_table(load_table_data)
+        except:
+            pass
+
+    def save_button_handler(self):
+        """
+        Обработчик кнопки сохранения
+        """
+        filename = self.saveFileDialog()
+        self.save_dataset_file(filename=filename)
+        
+
+    def graph_button_handler(self):
+        """
+        Обработчик кнопки создания графика
         """
         try:
             self.graphWidget.clear()
@@ -253,11 +314,41 @@ class main_window(Ui_MainWindow, QWidget):
             x = self.model.table[:,columns[-1]]
             y = self.model.table[:,columns[-2]]
             self.graphWidget.plot(x, y)
-
         except:
             pass
             #raise Exception('select 2 columns') 
     
+    def openFileNameDialog(self):  
+        """
+        Меню для открытия файла
+
+        return:
+            fileName : возвращает расположение файла
+        """  
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "",
+        "Hierarchical Data Format (*.hdf5)", options=options)
+        if fileName:
+            return fileName
+ 
+    def saveFileDialog(self):  
+        """
+        Меню для сохранения файла
+
+        return:
+            fileName : возвращает расположение файла
+        """   
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, format_file = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()", "",
+        "Hierarchical Data Format (*.hdf5)", options=options)
+        
+        if format_file == 'Hierarchical Data Format (*.hdf5)' and fileName[-5:] != '.hdf5':
+            fileName = fileName + '.hdf5'
+            return fileName
+        if format_file == 'Hierarchical Data Format (*.hdf5)' and fileName[-5:] == '.hdf5':
+            return fileName
     
         
 if __name__=="__main__":
---
+++ first/setting.py
@@ -0,0 +1,3 @@
+size_array = (10,10)
+preload_read_file = False
+preload_filename = 'first/file.hdf5'
